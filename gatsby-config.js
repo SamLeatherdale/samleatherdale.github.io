c09bdd67241644bcefb7a9e89fe6b774ddb9fde8
Sam Leatherdale <SamLeatherdale@users.noreply.github.com>
2019-09-25 16:53:52 +1000

    Update site icon and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,13 +28,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `samleatherdale.github.io`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#4b6dff`,
+        theme_color: `#4b6dff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/site-logo.png`, // This path is relative to the root of the site.
       },
     },
       `gatsby-plugin-sass`,
